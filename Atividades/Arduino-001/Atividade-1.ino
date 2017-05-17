48948e3b61641be16eae577ff8d73afd1860ded7
Italo Lelis <italohdc@gmail.com>
2017-05-16 21:55:20 -0300

    Resolução da Atividade 1, Arduino #001

---
+++ Atividades/Arduino-001/Atividade-1.ino
@@ -0,0 +1,56 @@
+/*
+	Arduino #001 - Atividade 1
+	Piscar LED em código morse com a mensagem "SOS"
+*/
+
+void setup ()
+{
+	pinMode(13, OUTPUT); // Declaração do pino 13 como saída
+}
+
+void loop ()
+{
+	// Primeiro loop para piscar o LED 3 vezes rapidamente
+	// S = * * *
+	for (int i=0; i<3; i++)
+	{
+		digitalWrite(13, HIGH);
+		delay(250);
+		digitalWrite(13, LOW);
+		delay(100);
+	}
+
+	// Pausa entre as letras
+	digitalWrite(13, LOW);
+	delay(200);
+
+
+	// Segundo loop para piscar o LED 3 vezes rapidamente
+	// S = * * *
+	for (int i=0; i<3; i++)
+	{
+		digitalWrite(13, HIGH);
+		delay(600);
+		digitalWrite(13, LOW);
+		delay(100);
+	}
+
+	// Pausa entre as letras
+	digitalWrite(13, LOW);
+	delay(200);
+
+
+	// Terceiro loop para piscar o LED 3 vezes rapidamente
+	// S = * * *
+	for (int i=0; i<3; i++)
+	{
+		digitalWrite(13, HIGH);
+		delay(250);
+		digitalWrite(13, LOW);
+		delay(100);
+	}
+
+	// Pausa final
+	digitalWrite(13, LOW);
+	delay(1000);
+}
